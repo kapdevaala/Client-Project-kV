16b025e6735247e40c09571316349aa6651500ee
kapdevaala <adops.kapdevaala@gmail.com>
2024-12-03 10:34:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Cart } from "./pages/Cart";
 
 function App() {
   return (
-    <div className="min-h-[100vh] max-w-[100vw] w-[100%]  bg-[#F2F0F1] border-2 border-black flex flex-col justify-start items-center">
+    <div className="min-h-[100vh] max-w-[100vw] w-[100%]  bg-[#F2F0F1] border-2 border-black hidden md:flex flex-col justify-start items-center">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
